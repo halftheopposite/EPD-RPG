b33cd17e0b8f83f28d9f402a6ae9f6bce5a270aa
Aymeric Chauvin <contact@halftheopposite.dev>
2025-04-07 22:58:35 +0200

    Add header to tool

--- editor/src/components/App.tsx
+++ editor/src/components/App.tsx
@@ -1,14 +1,18 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import { EditorProvider } from "../context/EditorContext";
+import Header from "./Header";
 import MainContent from "./MainContent";
 import Sidebar from "./Sidebar";
 
 function App(): ReactElement {
   return (
     <EditorProvider>
-      <div className="container">
-        <Sidebar />
-        <MainContent />
+      <div className="app-wrapper">
+        <Header />
+        <div className="container">
+          <Sidebar />
+          <MainContent />
+        </div>
       </div>
     </EditorProvider>
   );
